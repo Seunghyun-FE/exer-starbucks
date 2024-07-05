47e11062b90b8998b17f7c217413c5ea4728387e
sdsdsa <124785062+Seunghyun-FE@users.noreply.github.com>
2024-07-05 17:09:46 +0900

    promotion swiper&toggle

--- js/main.js
+++ js/main.js
@@ -41,9 +41,42 @@ fadeEls.forEach(function (fadeEl, index) {
   });
 });
 
-//swiper
+//new Swiper(선택자, 옵션)
 new Swiper(".notice-line .swiper-container", {
   direction: "vertical", // 수직 슬라이드
   autoplay: true, // 자동 재생 여부
   loop: true, // 반복 재생 여부
+});
+
+new Swiper(".promotion .swiper-container", {
+  slidesPerView: 3, // 보이는 슬라이드 수
+  spaceBetween: 10, // 간격
+  centeredSlides: true, // 1번 슬라이드가 가운데
+  loop: true, // 반복 재생 여부
+  // autoplay: {
+  //   delay: 3000,
+  // }, // 자동 재생 여부
+  pagination: {
+    el: ".promotion .swiper-pagination", //페이지 번호 요소 선택자
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
+});
+
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener("click", function () {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    //숨김
+    promotionEl.classList.add("hide");
+  } else {
+    //보임
+    promotionEl.classList.remove("hide");
+  }
 });
